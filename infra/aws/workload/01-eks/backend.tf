45a11833fa62236ae654269d406a5db5fab536d1
Randall Shutt <randall@randallman.net>
2026-01-05 02:05:14 -0500

    Relocating eks to 01-eks

--- infra/aws/workload/01-eks/backend.tf
+++ infra/aws/workload/01-eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tfstate-bucket-824123790769"
-    key          = "aws/eks/eks.tfstate"
+    key          = "aws/phase-2/eks.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
